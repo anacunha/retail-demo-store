6a5c964c4c629090282adcf70fa4f6e3dd7ebc54
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2021-09-30 19:14:01 -0300

    Add images to product and category data

--- src/products/convert_product_data.py
+++ src/products/convert_product_data.py
@@ -9,7 +9,16 @@ def format_product(product):
         product.pop('ibu')
     else:
         product['ibu'] = int(product['ibu'])
+    product['image'] = product['id'] + ".jpg"
     return product
+
+def create_category(id, product):
+    return {
+        'id': id,
+        'name': product['category'],
+        'image': product['image']
+    }
+
 
 if __name__=="__main__":
     beers_csv = open('beers.csv', 'r')
@@ -18,20 +27,14 @@ if __name__=="__main__":
 
     reader = csv.DictReader(beers_csv)
     products = list(reader)
+    categories = []
     category_names = set()
 
     for product in products:
         product = format_product(product)
-        category_names.add(product['category'])
+        if product['category'] not in category_names:
+            category_names.add(product['category'])
+            categories.append(create_category(len(category_names), product))
 
     yaml.dump(products, products_yaml, allow_unicode=True, sort_keys=False)
-
-    categories = []
-    for category_name in sorted(category_names):
-        category = {
-            'id': str(uuid.uuid4()),
-            'name': category_name
-        }
-        categories.append(category)
-
     yaml.dump(categories, categories_yaml, allow_unicode=True, sort_keys=False)
