923ce5599701bc000ac2e85c588bbf30447332f5
Jenna Pederson <jennamp@amazon.com>
2021-10-06 21:54:31 -0500

    Index locations by beer name

--- src/location/src/location-service/app.py
+++ src/location/src/location-service/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask import jsonify
+from flask import request
 from flask_cors import CORS
 
 import json
@@ -12,6 +13,7 @@ import pprint
 RESOURCE_BUCKET = os.environ.get('RESOURCE_BUCKET')
 
 locations = {}
+locationsByBeer = {}
 
 
 def load_data():
@@ -20,6 +22,16 @@ def load_data():
         locations_data = location_file_obj.read()
     locations = json.loads(locations_data)
 
+    for location in locations:
+        for key, value in location.items():
+            if key == "Beers":
+                for beer in value:
+                    if beer in locationsByBeer:
+                        locations = locationsByBeer[beer]
+                    else:
+                        locations = []
+                    locations.append(location)
+                    locationsByBeer[beer] = locations
 
 # -- Logging
 class LoggingMiddleware(object):
@@ -37,11 +49,31 @@ class LoggingMiddleware(object):
         return self._app(environ, log_response)
 # -- End Logging
 
+# -- Exceptions
+class BadRequest(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 # -- Handlers
 app = Flask(__name__)
 corps = CORS(app)
 
+@app.errorhandler(BadRequest)
+def handle_bad_request(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 @app.route('/')
 def index():
@@ -51,6 +83,17 @@ def index():
 @app.route('/locations/all')
 def get_locations():
     return jsonify(locations)
+
+@app.route('/locations')
+def get_locations_by_beer():
+    beer_name = request.args.get('beerName')
+    if not beer_name:
+        raise BadRequest('beerName is required')
+
+    if not beer_name in locationsByBeer:
+        raise BadRequest('beerName not found')
+
+    return jsonify(locationsByBeer[beer_name])
 
 
 if __name__ == '__main__':
